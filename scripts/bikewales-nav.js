d7ba7bb4773620c292d23b15d89b75aaf0fc9c2d
3291design - Architect <bikewales99@gmail.com>
2026-05-29 12:00:45 +0100

    Reintroduce Goals & Achievements dropdown menu

--- scripts/bikewales-nav.js
+++ scripts/bikewales-nav.js
@@ -51,18 +51,6 @@
             </div>
             <a href="https://bikewalesrides-home.blogspot.com/2022/01/rides-to-remember.html">Rides to Remember</a>
             <a href="https://bikewalesrides-home.blogspot.com/2022/01/rides-to-z_10.html">Ride Reports: A to Z</a>
-            <!-- <hr class="nav-hr" />
-            <div class="dropbtn2"><a href="#">&nbsp;&nbsp;&#9658;&nbsp;Goals &amp; Achievements:</a>
-                <div class="dropdown-content2">
-                    <a href="https://bikewalesrides-home.blogspot.com/p/2026-goals_23.html"><b>⤷ 2026</b> Goals</a>
-                    <a href="https://bikewalesrides-home.blogspot.com/p/2025-achievements.html"><b>2025</b> Achievements</a>
-                    <a href="https://bikewalesrides-home.blogspot.com/p/2025-goals.html"><b>⤷ 2025</b> Goals</a>
-                    <a href="https://bikewalesrides-home.blogspot.com/p/2024-achievements.html"><b>2024</b> Achievements</a>
-                    <a href="https://bikewalesrides-home.blogspot.com/p/2024-goals.html"><b>⤷ 2024</b> Goals</a>
-                    <a href="https://bikewalesrides-home.blogspot.com/p/2023-achievements.html"><b>2023</b> Achievements</a>
-                    <a href="https://bikewalesrides-home.blogspot.com/p/2022-achievements.html"><b>2022</b> Achievements</a>
-                </div>
-            </div> -->
             <hr class="nav-hr" />
             <a href="https://bikewalesrides-archive.blogspot.com">Ride Archive: 2012 - 2020</a>
         </div>
@@ -173,6 +161,17 @@
                 <div class="dropdown-content2" style="min-width: 210px;">
                     <a href="https://bikewalesgear-home.blogspot.com/2026/04/online-project-list.html">Online Project List</a>
                     <a href="https://bikewalesgear-home.blogspot.com/2026/04/ride-reports.html">Outstanding: Ride Reports</a>
+                    <div class="dropbtn2"><a href="#">&nbsp;&nbsp;&#9658;&nbsp;Goals &amp; Achievements:</a>
+                        <div class="dropdown-content2">
+                            <a href="https://bikewalesrides-home.blogspot.com/p/2026-goals_23.html"><b>⤷ 2026</b> Goals</a>
+                            <a href="https://bikewalesrides-home.blogspot.com/p/2025-achievements.html"><b>2025</b> Achievements</a>
+                            <a href="https://bikewalesrides-home.blogspot.com/p/2025-goals.html"><b>⤷ 2025</b> Goals</a>
+                            <a href="https://bikewalesrides-home.blogspot.com/p/2024-achievements.html"><b>2024</b> Achievements</a>
+                            <a href="https://bikewalesrides-home.blogspot.com/p/2024-goals.html"><b>⤷ 2024</b> Goals</a>
+                            <a href="https://bikewalesrides-home.blogspot.com/p/2023-achievements.html"><b>2023</b> Achievements</a>
+                            <a href="https://bikewalesrides-home.blogspot.com/p/2022-achievements.html"><b>2022</b> Achievements</a>
+                        </div>
+                    </div>
                     <a href="https://bikewalesgear-home.blogspot.com/2026/04/routes-to-ride.html">Future: Routes to Ride</a>
                     <a href="https://bikewalesgear-home.blogspot.com/2026/04/future-places-to-visit.html">Future: Places to Visit</a>
                 </div>
